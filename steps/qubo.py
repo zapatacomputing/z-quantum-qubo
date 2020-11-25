2f9f7fdf3829f4ce7d8ba852d9dd48e7c9115a85
michalstechly <michal.stechly@gmail.com>
2020-11-25 14:00:23 -0500

    Updated steps.

--- steps/qubo.py
+++ steps/qubo.py
@@ -1,12 +1,16 @@
 from dimod import generators, BinaryQuadraticModel, ExactSolver
 from zquantum.qubo import save_qubo, load_qubo
+from zquantum.core.measurement import Measurements
+
 
 def generate_random_qubo(size:int, seed:int=None):
-    qubo = generators(size, 'BINARY', seed=seed)
+    qubo = generators.uniform(size, 'BINARY', low=-1, high=1, seed=seed)
     save_qubo(qubo, "qubo.json")
 
 
 def get_exact_qubo_solution(qubo_json):
     qubo = load_qubo(qubo_json)
-    sampleset = ExactSolver().sample_qubo(qubo)
-    return sampleset
+    sampleset = ExactSolver().sample(qubo)
+    best_sample_dict = sampleset.first.sample
+    solution_bitstring = tuple(best_sample_dict[i] for i in sorted(best_sample_dict))
+    Measurements([solution_bitstring]).save("solution.json")
