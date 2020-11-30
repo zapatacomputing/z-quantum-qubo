61d22be450448b1e71cd222e1fecb069cff1f9af
michalstechly <michal.stechly@gmail.com>
2020-11-30 12:29:19 -0500

    Fixed packages in setup.py.

--- setup.py
+++ setup.py
@@ -8,8 +8,9 @@ setuptools.setup(
     author_email="info@zapatacomputing.com",
     description="A library containing logic for manipulating QUBOs for Orquestra.",
     url="https://github.com/zapatacomputing/z-quantum-qubo",
-    packages=setuptools.find_namespace_packages(include=["zquantum.*"]),
-    namespace_packages=setuptools.find_namespace_packages(include=["zquantum.*"]),
+    packages=[
+        "zquantum.qubo",
+    ],
     package_dir={"": "src/python"},
     classifiers=(
         "Programming Language :: Python :: 3",
