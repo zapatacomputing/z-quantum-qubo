852ebd7191e05b4a3f1acb4faa8cadb10518890d
Michał Stęchły <michal.stechly@gmail.com>
2021-01-14 15:52:22 -0500

    Merge dev into master (#5)
    * Initial code with basic functionalities.
    
    * Implement converting BQM to serializable
    
    * Cleanup tests and implementation of save_qubo and load_qubo
    
    * Renamed evaluations to utils.
    
    * Fix in io test.
    
    * Updated gitignore.
    
    * Updated conversion tests.
    
    * Updated steps.
    
    * Blackified code.
    
    * 175937967 fix installation bug (#3)
    
    * Added packages to setup.
    
    * Added namespace_packages to setup.
    
    * Fixed packages in setup.py.
    
    * Added where to packages in setup.py.
    
    * Workflow fix (#4)
    
    * Added packages to setup.
    
    * Added namespace_packages to setup.
    
    * Fixed packages in setup.py.
    
    * Added where to packages in setup.py.
    
    * Updated steps.
    
    * Added examples.
    
    * Changed output file name
    
    * Updated workflow.yaml.
    
    * Fixed a typo.
    
    Co-authored-by: dexter2206 <dexter2206@gmail.com>

---
+++ src/python/zquantum/qubo/__init__.py
@@ -0,0 +1,6 @@
+from .conversions import (
+    convert_qubo_to_openfermion_ising,
+    convert_openfermion_ising_to_qubo,
+)
+from .utils import evaluate_bitstring_for_qubo
+from .io import save_qubo, load_qubo
---
+++ steps/qubo.py
@@ -0,0 +1,25 @@
+from dimod import generators, BinaryQuadraticModel, ExactSolver
+from zquantum.qubo import save_qubo, load_qubo
+from zquantum.core.measurement import Measurements
+
+
+def generate_random_qubo(size: int, seed: int = None):
+    """Generates qubo with random parameters from a uniform distribution.
+    Args:
+        size: number of variables in qubo
+        seed: seed used for RNG
+    """
+    qubo = generators.uniform(size, "BINARY", low=-1, high=1, seed=seed)
+    save_qubo(qubo, "qubo.json")
+
+
+def get_exact_qubo_solution(qubo):
+    """Solves qubo by iterating over all the possible solutions.
+    Args:
+        qubo: qubo stored as a json
+    """
+    qubo = load_qubo(qubo)
+    sampleset = ExactSolver().sample(qubo)
+    best_sample_dict = sampleset.first.sample
+    solution_bitstring = tuple(best_sample_dict[i] for i in sorted(best_sample_dict))
+    Measurements([solution_bitstring]).save("exact_solution.json")
