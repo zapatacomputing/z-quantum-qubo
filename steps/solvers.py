c91c7e60d2c0163ebdd7025d0755aef1d8151a3f
mstechly <michal.stechly@gmail.com>
2021-01-29 14:13:35 -0500

    Fixed dict creation in steps/solvers.py .

--- steps/solvers.py
+++ steps/solvers.py
@@ -5,8 +5,10 @@ from zquantum.qubo import load_qubo
 
 def solve_qubo(qubo, solver_specs, sample_params):
     """Solves qubo using any sampler implementing either dimod.Sampler or zquantum.qubo.BQMSolver"""
-    solver_specs_dict = yaml.load(solver_specs, Loader=yaml.SafeLoader)
-    sample_params_dict = yaml.load(sample_params, Loader=yaml.SafeLoader)
+    solver_specs_dict = yaml.load(solver_specs, Loader=yaml.SafeLoader)["solver_specs"]
+    sample_params_dict = yaml.load(sample_params, Loader=yaml.SafeLoader)[
+        "sample_params"
+    ]
     solver = create_object(solver_specs_dict)
     qubo = load_qubo(qubo)
     sampleset = solver.sample(qubo, **sample_params_dict)
