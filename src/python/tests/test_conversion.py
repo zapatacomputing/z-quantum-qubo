6e6e134cac096e50973390abd2185b1e103265e6
mstechly <michal.stechly@gmail.com>
2021-02-11 18:00:46 -0500

    Added conversion between SampleSet and Measurements.

--- src/python/tests/test_conversion.py
+++ src/python/tests/test_conversion.py
@@ -1,10 +1,14 @@
 import numpy as np
 from openfermion import IsingOperator
+import dimod
+
 from zquantum.qubo.conversions import (
     convert_qubo_to_openfermion_ising,
     convert_openfermion_ising_to_qubo,
+    convert_sampleset_to_measurements,
+    convert_measurements_to_sampleset,
 )
-import dimod
+from zquantum.core.measurement import Measurements
 
 
 def test_qubo_with_binary_fractions():
@@ -40,3 +44,78 @@ def test_qubo_with_non_binary_fractions():
 
     for key in qubo.quadratic.keys():
         assert np.isclose(qubo.quadratic[key], new_qubo.quadratic[key])
+
+
+def test_convert_sampleset_to_measurements():
+    bitstrings = [
+        (0, 0, 0),
+        (0, 0, 1),
+        (0, 1, 0),
+        (0, 1, 1),
+        (1, 0, 0),
+        (1, 1, 0),
+        (1, 1, 1),
+        (1, 0, 1),
+        (0, 0, 1),
+    ]
+    energies = [0 for i in range(len(bitstrings))]
+    sampleset = dimod.SampleSet.from_samples(bitstrings, dimod.BINARY, energies)
+    target_measurements = Measurements(bitstrings)
+    converted_measurements = convert_sampleset_to_measurements(sampleset)
+
+    assert converted_measurements.bitstrings == target_measurements.bitstrings
+
+
+def test_convert_measurements_to_sampleset_without_qubo():
+    bitstrings = [
+        (0, 0, 0),
+        (0, 0, 1),
+        (0, 1, 0),
+        (0, 1, 1),
+        (1, 0, 0),
+        (1, 1, 0),
+        (1, 1, 1),
+        (1, 0, 1),
+        (0, 0, 1),
+    ]
+    measurements = Measurements(bitstrings)
+
+    target_sampleset = dimod.SampleSet.from_samples(
+        bitstrings, dimod.BINARY, [np.nan for _ in bitstrings]
+    )
+    converted_sampleset = convert_measurements_to_sampleset(measurements)
+
+    # Since energies should be np.nans, using "==" will result in error
+    for (target_record, converted_record) in zip(
+        target_sampleset.record, converted_sampleset.record
+    ):
+        for target_element, converted_element in zip(target_record, converted_record):
+            np.testing.assert_equal(target_element, converted_element)
+
+    assert converted_sampleset.vartype == target_sampleset.vartype
+
+
+def test_convert_measurements_to_sampleset_with_qubo():
+    bitstrings = [
+        (0, 0, 0),
+        (0, 0, 1),
+        (0, 1, 0),
+        (0, 1, 1),
+        (1, 0, 0),
+        (1, 1, 0),
+        (1, 1, 1),
+        (1, 0, 1),
+        (0, 0, 1),
+    ]
+    qubo = dimod.BinaryQuadraticModel(
+        {0: 1, 1: 2, 2: 3},
+        {(1, 2): 0.5, (1, 0): -0.25, (0, 2): 2.125},
+        0,
+        vartype=dimod.BINARY,
+    )
+    energies = [0, 3, 2, 5.5, 1, 2.75, 8.375, 6.125, 3]
+    measurements = Measurements(bitstrings)
+
+    target_sampleset = dimod.SampleSet.from_samples(bitstrings, dimod.BINARY, energies)
+    converted_sampleset = convert_measurements_to_sampleset(measurements, qubo)
+    assert target_sampleset == converted_sampleset
