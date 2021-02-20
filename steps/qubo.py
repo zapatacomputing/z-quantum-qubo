bae139e87c06e514e8a54fd2f9c5a8845d616995
Guoming Wang <guoming.wang@zapatacomputing.com>
2021-02-20 00:26:18 -0500

    Added steps get_qubo_hamiltonian and convert_measurements_to_sampleset

--- steps/qubo.py
+++ steps/qubo.py
@@ -1,5 +1,7 @@
 from dimod import generators, BinaryQuadraticModel, ExactSolver
-from zquantum.qubo import save_qubo, load_qubo
+from zquantum.qubo import (save_qubo, load_qubo, save_sampleset, convert_qubo_to_openfermion_ising,
+                            convert_measurements_to_sampleset as _convert_measurements_to_sampleset)
+
 from zquantum.core.measurement import Measurements
 from zquantum.core.utils import SCHEMA_VERSION
 
@@ -12,3 +14,20 @@ def generate_random_qubo(size: int, seed: int = None):
     """
     qubo = generators.uniform(size, "BINARY", low=-1, high=1, seed=seed)
     save_qubo(qubo, "qubo.json")
+
+
+def get_qubo_hamiltonian(qubo):
+    """Convert qubo to Ising Hamiltonian.
+    Args:
+        qubo: qubo stored as a json
+    """
+    qubo = load_qubo(qubo)
+    hamiltonian = convert_qubo_to_openfermion_ising(qubo)
+    save_ising_operator(hamiltonian, "hamiltonian.json")
+
+
+def convert_measurements_to_sampleset(measurements, qubo):
+    qubo = load_qubo(qubo)
+    measurements = Measurements.load_from_file(measurements)
+    sampleset = _convert_measurements_to_sampleset(measurements, qubo)
+    save_sampleset(sampleset, "sampleset.json")
