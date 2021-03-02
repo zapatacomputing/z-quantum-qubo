b60078bd1a229eda847799fe52c7de986fc75c81
mstechly <michal.stechly@gmail.com>
2021-03-02 16:56:46 -0500

    Refactored sampler_params to solver_params.

--- steps/solvers.py
+++ steps/solvers.py
@@ -6,18 +6,18 @@ from zquantum.core.measurement import Measurements
 from zquantum.core.utils import ValueEstimate, save_value_estimate
 
 
-def solve_qubo(qubo, solver_specs, sampler_params=None):
+def solve_qubo(qubo, solver_specs, solver_params=None):
     """Solves qubo using any sampler implementing either dimod.Sampler or zquantum.qubo.BQMSolver"""
-    if sampler_params is None:
-        sampler_params = {}
+    if solver_params is None:
+        solver_params = {}
     solver = create_object(solver_specs)
     qubo = load_qubo(qubo)
 
-    sampleset = solver.sample(qubo, **sampler_params)
+    sampleset = solver.sample(qubo, **solver_params)
     best_sample_dict = sampleset.first.sample
-    solution_bitstring = tuple(best_sample_dict[i] for i in sorted(best_sample_dict))    
+    solution_bitstring = tuple(best_sample_dict[i] for i in sorted(best_sample_dict))
     lowest_energy = evaluate_bitstring_for_qubo(solution_bitstring, qubo)
-    
-    save_value_estimate(ValueEstimate(lowest_energy), "lowest-energy.json")    
+
+    save_value_estimate(ValueEstimate(lowest_energy), "lowest-energy.json")
     Measurements([solution_bitstring]).save("solution.json")
     save_sampleset(sampleset, "sampleset.json")
