0d29515d88bfce9fe3348909a36a2f6af9273eef
mstechly <michal.stechly@gmail.com>
2021-10-28 11:58:15 -0400

    Fixed mypy issues.

--- src/python/zquantum/qubo/__init__.py
+++ src/python/zquantum/qubo/__init__.py
@@ -1,7 +1,7 @@
 from .conversions import (
     convert_qubo_to_openfermion_ising,
     convert_openfermion_ising_to_qubo,
-    convert_measurements_to_sampleset
+    convert_measurements_to_sampleset,
 )
 from .utils import evaluate_bitstring_for_qubo
 from .io import save_qubo, load_qubo, save_sampleset, load_sampleset
