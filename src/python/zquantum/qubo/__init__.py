64b39b036c2215919f1b326e00f31cc48758dbc6
mstechly <michal.stechly@gmail.com>
2021-11-30 10:48:14 -0500

    Fixed isort issue.

--- src/python/zquantum/qubo/__init__.py
+++ src/python/zquantum/qubo/__init__.py
@@ -1,8 +1,8 @@
 from .conversions import (
     convert_measurements_to_sampleset,
-    convert_sampleset_to_measurements,
     convert_openfermion_ising_to_qubo,
     convert_qubo_to_openfermion_ising,
+    convert_sampleset_to_measurements,
 )
 from .io import load_qubo, load_sampleset, save_qubo, save_sampleset
 from .utils import evaluate_bitstring_for_qubo
